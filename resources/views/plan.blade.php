3c82d3a75bb4f0fbbe5d01110061301cef410a85
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-09-29 20:57:01 +0900

    会員プランページを作成

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.website')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/first.css') }}?time={{ time() }}" type='text/css' media='all' />
+    <link rel="stylesheet" href="{{ asset('css/plan.css') }}?time={{ time() }}" type='text/css' media='all' />
 @endpush
 
 @section('content')
@@ -10,25 +10,86 @@
     'screen_data' => [
         'title' => '会員プラン',
         'title_en' => 'Member plan',
+        'background_image' => asset('images/inner-hero/会員プラン.jpg'),
     ]
 ])
 
-{{-- 見学・無料体験 --}}
-<section class="">
+@php
+    $plans = [
+        [
+            'title' => 'フルタイムコース',
+            'image' => asset('images/plan/男性フルタイム会員.jpg'),
+            'target' => '18歳以上の方',
+            'admission_fee' => '11,000円',
+            'monthly_fee' => '19,800円',
+        ], [
+            'title' => '8Daysコース',
+            'image' => asset('images/plan/男性８デイズ会員.jpg'),
+            'target' => '18歳以上の方',
+            'admission_fee' => '11,000円',
+            'monthly_fee' => '17,600円',
+        ], [
+            'title' => '学生コース',
+            'image' => asset('images/plan/学生会員.jpg'),
+            'target' => '中学生〜大学4年生',
+            'admission_fee' => '11,000円',
+            'monthly_fee' => '17,600円',
+        ], [
+            'title' => 'キッズコース',
+            'image' => asset('images/plan/キッズ会員.jpg'),
+            'target' => '5歳〜小学6年生',
+            'admission_fee' => '11,000円',
+            'monthly_fee' => '17,600円',
+        ], [
+            'title' => '法人コース',
+            'image' => asset('images/plan/法人会員.jpg'),
+            'target' => '法人・団体の方5名～20名',
+            'admission_fee' => '11,000円',
+            'monthly_fee' => '77,000円（5名）～275,000円（20名）',
+        ],
+    ];
+@endphp
+
+{{-- 各種プラン --}}
+<section class="plan-section">
     <div class="content-wrapper">
         <div class="section-title">
-            <h3 class="">見学・無料体験</h3>
+            <h3 class="">各種プラン</h3>
         </div>
-        <div class="section-body" style="max-width: 650px; margin: 0 auto;">
-            <p>「柔術ってどんなスポーツ？」</p>
-            <p>「道場の雰囲気を見てみたい」</p>
-            <p>「実際に柔術を体験してみたい」</p>
-            <p>そんな方々の為にも、カルペディエム三田では、見学・体験を受け付けております！</p>
-            <p>事前に問い合わせの申込フォームからご予約ください。</p>
-            <p>レンタル胴衣も無料でお貸しする事が出来ます。どのクラスでも可能ですし、トレーニング中の強制等はございません。お気軽に足をお運びくださいませ。</p>
-            <p>※ご予約の上、クラス開始10分前に来館ください。</p>
-            <p>※ブラジリアン柔術経験者、また入会の意思がない方はビジター料￥4,000をお支払い頂きます。</p>
+        <div class="plan-intro">
+            <p>ご入会時には下記費用が必要になります。当ジムでのご入会手続き時にお支払いをお願い致します。</p>
+            <ul class="plan-intro-list">
+                <li>ご入会金</li>
+                <li>当月会費</li>
+                <li>翌月会費</li>
+            </ul>
+            {{-- <p>各会員のご入会時に必要な費用については、各会員ページ内にある「ご入会について」をご確認ください。</p> --}}
         </div>
+        @foreach ($plans as $plan)
+            <div class="plan-card">
+                <div class="plan-hero" style="background-image: url('{{ $plan['image'] }}');">
+                    <h2 class="plan-hero-title">{{ $plan['title'] }}</h2>
+                </div>
+                <div class="plan-table-wrap">
+                    <table class="plan-table">
+                        <tbody>
+                            <tr>
+                                <th>対象</th>
+                                <td>{{ $plan['target'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>入会金</th>
+                                <td>{{ $plan['admission_fee'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>月会費</th>
+                                <td>{{ $plan['monthly_fee'] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endforeach
     </div>
 </section>
 
